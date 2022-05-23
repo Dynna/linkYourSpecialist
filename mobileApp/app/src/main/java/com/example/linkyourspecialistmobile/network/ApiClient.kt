830cf5e3326fd016527ab3c0b79393213882f2e2
ecaterinagherbovei <catea.gherbovei@gmail.com>
2022-05-23 20:04:43 +0300

    some changes in mobile app

--- mobileApp/app/src/main/java/com/example/linkyourspecialistmobile/network/ApiClient.kt
+++ mobileApp/app/src/main/java/com/example/linkyourspecialistmobile/network/ApiClient.kt
@@ -7,8 +7,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-const val BASEURL = "http://10.0.2.2:4002"
-//const val BASEURL = "http://192.168.100.10:4002"
+//const val BASEURL = "http://10.0.2.2:4002"
+const val BASEURL = "https://link-your-specialist-backend2.herokuapp.com"
 
 class ApiClient {
     companion object {
